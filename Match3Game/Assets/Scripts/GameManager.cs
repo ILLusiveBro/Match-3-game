6b4cd45e06f6779e4198583d82fc2a0ea37b693d
Igor <igor.lviv96@gmail.com>
2017-09-08 21:08:51 +0300

    Added game field

---
+++ Match3Game/Assets/Scripts/GameManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public Sprite spriteGreen;
+    public Sprite spriteBlue;
+    public Sprite spritePurple;
+    public Sprite spriteRed;
+    public Sprite spriteOrange;
+    List<Sprite> sprites = new List<Sprite>();
+
+    void Start()
+    {
+        sprites.Add(spriteGreen);
+        sprites.Add(spriteBlue);
+        sprites.Add(spritePurple);
+        sprites.Add(spriteRed);
+        sprites.Add(spriteOrange);
+        Screen.SetResolution(600, 800, false);
+        foreach(var tiles in GameObject.FindGameObjectsWithTag("Tiles"))
+        {         
+            tiles.GetComponent<SpriteRenderer>().sprite = sprites[Random.Range(0,sprites.Count)];
+        }
+    }
+}
